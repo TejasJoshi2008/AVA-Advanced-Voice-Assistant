9d18facad50b444902755f03eec4dc8e75628560
Tejas Joshi <tejassamurai@gmail.com>
2020-11-04 16:11:19 +0530

    I did some other minor changes such as fixed spacing

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 import sqlite3
+
 def connection_on():
     connection  = sqlite3.connect("commands.db")
     return connection
+
 def get_question():
     con = connection_on()
     cur = con.cursor()
     cur.execute("SELECT*from questionANDanswer")
     return cur.fetchall()
+
 def insert(question,answer):
     con = connection_on()
     cur = con.cursor()
     que = f"INSERT INTO questionANDanswer values('"+question+"','"+answer+"')"
     cur.execute(que)
     con.commit()
+
 def get_answer(question):
     rows = get_question()
     answer = ""
